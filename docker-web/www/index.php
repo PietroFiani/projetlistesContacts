952ff9621d69a27d40ecb82b0521cf0520daa276
pepito's <fiani.pietro@gmail.com>
2020-09-16 11:47:23 +0200

    db connection test

--- docker-web/www/index.php
+++ docker-web/www/index.php
@@ -1,1 +1,19 @@
-<?php phpinfo(); 
+<?php
+$bdd = new PDO('mysql:host=mySql;dbname=testcontact;charset=utf8', 'root', 'pepito');
+
+
+
+$sql = "SELECT firstname FROM users";
+$result = $bdd->query($sql);  #les problemes
+
+// if ($result->num_rows > 0) {
+// 	// output data of each row
+// 	while($row = $result->fetch_assoc()) {
+// 		echo $row['name']."&lt;br>";
+// 	}
+// } else {
+// 	echo "0 results";
+// }
+// $bdd->close();
+
+?>
