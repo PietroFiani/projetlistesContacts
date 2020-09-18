5b9873553fdca95eee9153b40d17d4d28684a3d4
pepito's <fiani.pietro@gmail.com>
2020-09-18 10:52:15 +0200

    requestmaj

--- docker-web/www/model.php
+++ docker-web/www/model.php
@@ -11,8 +11,6 @@
 
 //}
 
-?>
-
 <?php
 
 function getinfos()
@@ -42,8 +40,31 @@ function insertcontact()
 	    die('Erreur : '.$e->getMessage());
 	}
 
-	$req = $db->query('SELECT firstname,lastname,mail,phone FROM contacts)');
-
-	return $req;
+	$req = $db->prepare('INSERT INTO contacts (`id`, `firstname`, `lastname`, `mail`, `phone`, `prop_mail`) VALUES (NULL, :firstname, :lastname, :mail, :phone, :prop_mail)');
+    $req->execute(array(
+	'firstname ' => $firstname,
+	'lastname' => $lastname,
+	'mail' => $mail,
+    'phone' => $phone,
+	'prop_mail' => $pro_mail,
+	));
+	
+echo 'Le contact a bien été ajouté !';
 }
-?>
+/*?>
+/*<?php
+/*function deletecontact()
+/*{
+/*    try
+/*    {
+/*        $db = new PDO('mysql:host= $servername; dbname= $dbname;charset=utf8', '$username', '$username');
+/*    }
+/*    catch (Exception $e)
+/*    {
+/*       die('Erreur :' .$e->getMessage()); 
+/*    }
+/*  $req = $db->prepare('DELETE FROM contacts WHERE firstname=$firstname lastname=$lastname'); 
+/*      $req->execute()
+/*    )); 
+/*}
+/*?> */
