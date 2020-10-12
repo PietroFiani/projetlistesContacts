5276f081e45ce01b93890d587146dc754c6d70de
pelinski <thibault.pelinski10@gmail.com>
2020-10-13 00:27:34 +0200

    WIP: front interface

--- docker-web/www/View.php
+++ docker-web/www/View.php
@@ -1,31 +1,65 @@
 <!DOCTYPE html>
+
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="test.css">
+    <!-- <link rel="stylesheet" href="test.css"> -->
+    <!-- CSS only -->
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css"
+        integrity="sha384-JcKb8q3iqJ61gNV9KGb8thSsNjpSL0n8PARn9HuZOnIxN0hoP+VmmDGMN5t9UJ0Z" crossorigin="anonymous">
+
+    <!-- JS, Popper.js, and jQuery -->
+    <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js"
+        integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous">
+    </script>
+    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js"
+        integrity="sha384-9/reFTGAW83EW2RDu2S0VKaIzap3H66lZH81PoYlFhbGU+6BZp6G7niu735Sk7lN" crossorigin="anonymous">
+    </script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"
+        integrity="sha384-B4gt1jrGC7Jh4AgTPSdUtOBvfO8shuf57BaghqFfPlYxofvL8/KUEfYiJOMMV+rV" crossorigin="anonymous">
+    </script>
     <title>Accueil</title>
 </head>
+
 <body>
-    
+
     <header>
 
 
     </header>
 
     <div class="main section-nav">
-
-    <button name="insert-contact" id="insert-contact-btn">Ajouter</button>
-        
-        <div class="insert-contact-container">
-            <form class="form-" action="./controllers/insertContact.php" method="post"> 
-                <input type="text" name="firstname" placeholder="prénom">
-                <input type="text" name="lastname" placeholder="nom">
-                <input type="email" name="mail" placeholder="mail">
-                <input type="tel" name="phone" placeholder="téléphone">
-                <button  type="submit"> inserer contact</button>
-            </form>
+        <nav class="navbar navbar-dark bg-primary">
+            <div class="container">
+                <a class="navbar-brand" href="#">Liste de Contact</a>
+                <button class="btn btn-light my-2 my-sm-0" data-toggle="modal" data-target="#modalAdd">Ajouter</button>
+            </div>
+        </nav>
+        <div class="modal fade" id="modalAdd" tabindex="-1" role="dialog" aria-labelledby="modalAdd" aria-hidden="true">
+            <div class="modal-dialog" role="document">
+                <div class="modal-content">
+                    <div class="insert-contact-container">
+                        <div class="modal-body">
+                            <form class="form-" action="./controllers/insertContact.php" method="post">
+                            <label>Prénom </label>
+                                <input type="text" class="form-control" name="firstname">
+                            <label>Nom</label>
+                                <input type="text" class="form-control" name="lastname">
+                            <label>Email</label>
+                                <input type="email" class="form-control" name="mail">
+                            <label>Téléphone</label>
+                                <input type="tel" name="tel" class="form-control">
+                                <button type="submit" class="btn btn-primary" style="margin-top:20px;">Inserer contact</button>
+                                <button type="button" class="btn btn-secondary" data-dismiss="modal" style="margin-top:20px;"> Annuler </button>
+                            </form>
+                        </div>
+                    </div>
+                </div>
+            </div>
         </div>
+
 
         <div class="research-contact-container">
             <form action="./controllers/researchContact.php" method="post">
@@ -33,48 +67,59 @@
                 <button type="submit"> rechercher </button>
             </form>
         </div>
-    
-    </div>
 
 
-        
 
-    <?php include_once('./controllers/getInfos.php'); ?>
+
+
+        <?php include_once('./controllers/getInfos.php'); ?>
 
         <?php foreach ($infos as $info): ?>
+        <div class="col-sm">
+            <div class="card">
+                <div class="card-body">
 
-            <section class="card">
-                
-                <h5 class="card-text"><?php echo $info['firstname']?></h5>
-                <h5 class="card-text"><?php echo $info['lastname']?></h5>
-                <p class="card-text"><?php echo $info ['mail']?></p>
-                <p class="card-text"><?php echo $info['phone']?></p>
-                
-                <form action='./controllers/deleteContacts.php' method='post' class="deleteContact-form">
-                    <input type="hidden" name="id" value= "<?php echo $info['id'];?>"/>
-                    <button  type="submit" class="delete-btn"> Supprimer contact</button>
-                </form>
+                    <h5 class="card-text"><?php echo $info['firstname']?></h5>
+                    <h5 class="card-text"><?php echo $info['lastname']?></h5>
+                    <p class="card-text"><?php echo $info ['mail']?></p>
+                    <p class="card-text"><?php echo $info['phone']?></p>
 
-            </section>
+                    <form action='./controllers/deleteContacts.php' method='post' class="deleteContact-form">
+                        <input type="hidden" name="id" value="<?php echo $info['id'];?>" />
+                        <button class="btn btn-primary" type="button" data-toggle="collapse"
+                            data-target="#collapseUpdateContact" aria-expanded="false"
+                            aria-controls="collapseUpdateContact">
+                            Modifier le contact</button>
+                        <button type="submit" class="delete-btn btn btn-danger"> Supprimer contact</button>
+                    </form>
 
-            <section class="update-form">
-                
-                <form class="form-update" action="./controllers/updateInfos.php" method="post"> 
-                    <input type="text" name="firstname">
-                    <input type="text" name="lastname">
-                    <input type="text" name="mail">
-                    <input type="text" name="phone">
-                    <input type="hidden" name="id" value= "<?php echo $info['id'];?>"/>
-                    <button  type="submit"> Mettre à jour contact</button>
-                </form>
 
-            </section>
-        
 
-        <?php endforeach; ?> 
+                    <div class="collapse" id="collapseUpdateContact">
+                        <form class="form-update" action="./controllers/updateInfos.php" method="post">
+                            <label for="inputPrenom" style="margin-top:20px;">Prénom</label>
+                            <input type="text" class="form-control" name="firstname">
+                            <label for="inputNom">Nom</label>
+                            <input type="text" class="form-control" name="lastname">
+                            <label for="inputMail">Mail</label>
+                            <input type="text" class="form-control" name="mail">
+                            <label for="inputPhone">Téléphone</label>
+                            <input type="text" class="form-control" name="phone">
+                            <input type="hidden" name="id" value="<?php echo $info['id'];?>" />
+                            <button type="submit" class="btn btn-primary" style="margin-top:20px;"> Mettre à jour
+                                contact</button>
+                        </form>
+                    </div>
+                </div>
+            </div>
+        </div>
 
+        <?php endforeach; ?>
     </div>
 
-<script src="index.js"></script>
+
+
+    <script src="index.js"></script>
 </body>
+
 </html>
