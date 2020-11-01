02ebe41e589e6334f1fe821bdb4c5fbcf1f855b2
PietroFiani <fiani.pietro@gmail.com>
2020-11-01 17:23:44 +0100

    Nettoyage

--- docker-web/www/index.js
+++
@@ -1,8 +0,0 @@
-const insertbtn = document.querySelector('#insert-contact-btn')
-const insertContactContainer = document.querySelector('.insert-contact-container')
-
-insertbtn.addEventListener ("click", () => {
-
-    insertContactContainer.classList.toggle('Blockaupire')
-
-})
